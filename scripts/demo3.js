fac2fd3c464f7dee206f65104fe77799b97ed157
xiaoheshang <741852440@qq.com>
2016-08-30 18:11:13 +0800

    事件类型知识点之前提交

--- scripts/demo3.js
+++ scripts/demo3.js
@@ -1,7 +1,56 @@
 var btn1 = document.getElementById('btn1');
 
 var handler = function() {
-    alert(event.target.id);
+    console.log(event.target.id);
 }
 
 EventUtil.addHandler(btn1, 'click', handler);
+
+var btn2 = document.getElementById('btn2');
+btn2.onclick = function(event) {
+    console.log('');
+    alert(this === event.currentTarget); //true
+    alert(this === event.target); //true
+}
+
+document.body.onclick = function(event) {
+    console.log('');
+    console.log(this === event.currentTarget); //true
+    console.log(this === document.body); //true
+    console.log(btn2 === event.target); //true
+};
+
+//使用 event.type
+var btn3 = document.getElementById('btn3');
+
+function handlerEvent() {
+    switch (event.type) {
+        case 'click':
+            alert('clicked');
+            break;
+        case 'mouseover':
+            alert('mouseover');
+            break;
+        case 'mouseout':
+            alert('mouseout');
+            break;
+    }
+}
+btn3.addEventListener('click', handlerEvent, false);
+btn3.addEventListener('mouseover', handlerEvent, false);
+btn3.addEventListener('mouseout', handlerEvent, false);
+
+//使用 event.preventDefault()阻止默认行为
+
+var link1 = document.getElementById('link1');
+link1.onclick = function() {
+    event.preventDefault();
+    console.log('不会跳转到指定连接地址');
+}
+
+//IE中的event对象
+var btn4 = document.getElementById('btn4');
+
+btn4.attachEvent('onclick', function() {
+    alert(event.type);
+})
--- scripts/public.js
+++ scripts/public.js
@@ -1,4 +1,5 @@
 //兼容性浏览器事件处理程序绑定和解除方法
+//增强添加兼容性事件对象,兼容IE8以下
 var EventUtil = {
     addHandler: function(element, type, handler) {
         if (element.addEventListener) {
@@ -17,6 +18,26 @@ var EventUtil = {
             element.detachEvent('on' + type, handler);
         } else {
             element['on' + type] = null;
+        }
+    },
+    getEvent: function(event) {
+        return event ? event : window.event;
+    },
+    getTarget: function(event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
         }
     }
 };
